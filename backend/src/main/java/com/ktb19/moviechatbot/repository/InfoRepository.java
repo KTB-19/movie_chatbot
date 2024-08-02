784ff65305798e8287bd4357a7ce36be1ae2b168
yohan <dygks924@gmail.com>
2024-08-02 20:52:08 +0900

    feat: InfoRepository findAllByQuery 구현

--- backend/src/main/java/com/ktb19/moviechatbot/repository/InfoRepository.java
+++ backend/src/main/java/com/ktb19/moviechatbot/repository/InfoRepository.java
@@ -11,6 +11,12 @@ import java.util.List;
 
 @Repository
 public interface InfoRepository extends JpaRepository<Info, Long> {
-    @Query("select i from Info i")
+    @Query("select i from Info i" +
+            " left join i.movie m" +
+            " left join i.theater t" +
+            " where m.title = :movieName" +
+            " and t.wideArea = :wideArea" +
+            " and t.basicArea = :basicArea" +
+            " and i.date = :date")
     List<Info> findAllByQuery(String movieName, String wideArea, String basicArea, LocalDate date);
 }

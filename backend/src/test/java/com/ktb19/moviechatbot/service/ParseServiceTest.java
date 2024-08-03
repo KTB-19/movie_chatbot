55341ff11683fa0fed9ea3817bd44836735694a3
yohan <dygks924@gmail.com>
2024-08-03 09:58:39 +0900

    test: ParseServiceTest update

--- backend/src/test/java/com/ktb19/moviechatbot/service/ParseServiceTest.java
+++ backend/src/test/java/com/ktb19/moviechatbot/service/ParseServiceTest.java
@@ -1,24 +1,19 @@
 package com.ktb19.moviechatbot.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ktb19.moviechatbot.dto.QueriesDto;
 import com.ktb19.moviechatbot.dto.QueryDto;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import com.ktb19.moviechatbot.exception.FailParsingPyObjectToJsonException;
+import com.ktb19.moviechatbot.exception.PyFunctionNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.python.core.PyFunction;
 import org.python.core.PyObject;
 import org.python.core.PyUnicode;
 import org.python.util.PythonInterpreter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
@@ -36,7 +31,7 @@ class ParseServiceTest {
 
     @Test
     @DisplayName("주어진 메시지를 파싱하여 QueryDto 객체로 반환한다.")
-    void testParse() throws JsonProcessingException {
+    void testParse() {
         // Given
         String message = "test message";
         String jsonString = "{\"movieName\": \"Test Movie\", \"region\": \"Test Region\", \"date\": \"2024-01-01\"}";
@@ -59,8 +54,43 @@ class ParseServiceTest {
     }
 
     @Test
+    @DisplayName("pyFunction null이면, PyFunctionNotFoundException 던진다")
+    void testParse_pyFunction_not_found() {
+        //Given
+        String message = "test message";
+
+        given(pythonInterpreter.get(anyString(), eq(PyFunction.class))).willReturn(null);
+
+        //When
+        //Then
+        assertThatThrownBy(() -> parseService.parse(message))
+                .isInstanceOf(PyFunctionNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("잘못된 형식의 pyObject가 반환되면, FailParsingPyObjectToJsonException 던진다")
+    void testParse_fail_parsing() {
+        //Given
+        String message = "test message";
+
+        String jsonString = "{\"wrongName\": \"Test Movie\", \"region\": \"Test Region\", \"date\": \"2024-01-01\"}";
+
+        PyFunction mockFunction = mock(PyFunction.class);
+        PyObject mockPyObject = new PyUnicode(jsonString);
+
+        given(pythonInterpreter.get(anyString(), eq(PyFunction.class))).willReturn(mockFunction);
+        given(mockFunction.__call__(any(PyObject.class))).willReturn(mockPyObject);
+        willDoNothing().given(pythonInterpreter).execfile("src/main/java/com/ktb19/moviechatbot/ai/test1.py");
+
+        //When
+        //Then
+        assertThatThrownBy(() -> parseService.parse(message))
+                .isInstanceOf(FailParsingPyObjectToJsonException.class);
+    }
+
+    @Test
     @DisplayName("추가 쿼리를 파싱하여 기존 parsedQuery와 병합된 QueryDto 객체로 반환한다.")
-    void testParseAdditional() throws JsonProcessingException {
+    void testParseAdditional() {
         // Given
         QueryDto parsedQuery = new QueryDto();
         parsedQuery.setMovieName("Parsed Movie");
@@ -86,5 +116,51 @@ class ParseServiceTest {
         assertThat(result.getMovieName()).isEqualTo("Parsed Movie"); // This should remain from parsedQuery
         assertThat(result.getRegion()).isEqualTo("New Region");
         assertThat(result.getDate()).isEqualTo("2024-02-02");
+    }
+
+    @Test
+    @DisplayName("pyFunction null이면, PyFunctionNotFoundException 던진다")
+    void testParseAdditional_pyFunction_not_found() {
+        //Given
+        QueryDto parsedQuery = new QueryDto();
+        parsedQuery.setMovieName("Parsed Movie");
+
+        QueriesDto additionQueries = new QueriesDto();
+        additionQueries.setMovieNameQuery("New Movie");
+        additionQueries.setRegionQuery("New Region");
+        additionQueries.setDateQuery("2024-02-02");
+
+        given(pythonInterpreter.get(anyString(), eq(PyFunction.class))).willReturn(null);
+
+        //When
+        //Then
+        assertThatThrownBy(() -> parseService.parseAdditional(parsedQuery, additionQueries))
+                .isInstanceOf(PyFunctionNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("잘못된 형식의 pyObject가 반환되면, FailParsingPyObjectToJsonException 던진다")
+    void testParseAdditional_fail_parsing() {
+        //Given
+        QueryDto parsedQuery = new QueryDto();
+        parsedQuery.setMovieName("Parsed Movie");
+
+        QueriesDto additionQueries = new QueriesDto();
+        additionQueries.setMovieNameQuery("New Movie");
+        additionQueries.setRegionQuery("New Region");
+        additionQueries.setDateQuery("2024-02-02");
+
+        String jsonString = "{\"wrongName\": \"New Movie\", \"region\": \"New Region\", \"date\": \"2024-02-02\"}";
+
+        PyFunction mockFunction = mock(PyFunction.class);
+        PyObject mockPyObject = new PyUnicode(jsonString);
+
+        given(mockFunction.__call__(any(PyObject.class), any(PyObject.class), any(PyObject.class))).willReturn(mockPyObject);
+        given(pythonInterpreter.get(anyString(), eq(PyFunction.class))).willReturn(mockFunction);
+
+        //When
+        //Then
+        assertThatThrownBy(() -> parseService.parseAdditional(parsedQuery, additionQueries))
+                .isInstanceOf(FailParsingPyObjectToJsonException.class);
     }
 }

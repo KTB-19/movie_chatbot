cc1d0b03c395858089f7a266fc76597a9467fc3c
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-07 15:10:04 +0900

    refactor: MovieController endpoint 수정

--- backend/src/main/java/com/ktb19/moviechatbot/controller/MovieController.java
+++ backend/src/main/java/com/ktb19/moviechatbot/controller/MovieController.java
@@ -7,14 +7,12 @@ import com.ktb19.moviechatbot.service.ParseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class MovieController {
 
     private final ParseService parseService;

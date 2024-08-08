bc30ddd65101c15bd408cd9e3aa50f7512261225
yohan <dygks924@gmail.com>
2024-08-08 13:17:07 +0900

    refactor: ErrorResponse timestamp 추가

--- backend/src/main/java/com/ktb19/moviechatbot/exception/common/ErrorResponse.java
+++ backend/src/main/java/com/ktb19/moviechatbot/exception/common/ErrorResponse.java
@@ -2,20 +2,26 @@ package com.ktb19.moviechatbot.exception.common;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 public class ErrorResponse {
 
     private final String code;
     private final String message;
+    private final String timestamp;
 
     public ErrorResponse(ErrorCode errorCode) {
         this.code = errorCode.name();
         this.message = errorCode.getMessage();
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
     }
 
     public ErrorResponse(final String code, final String message) {
         this.code = code;
         this.message = message;
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
     }
 
     public static ErrorResponse of(ErrorCode errorCode) {

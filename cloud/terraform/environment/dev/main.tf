5b0fc1c524f535289e6e125befdeb186ca36a651
dogyungkim <prunsoli12@gmail.com>
2024-08-13 17:12:10 +0900

    feat:ansible을 통한 docker 및 node-exporter 설치

--- cloud/terraform/environment/dev/main.tf
+++ cloud/terraform/environment/dev/main.tf
@@ -80,6 +80,13 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0

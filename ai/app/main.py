6842bed84e08e64e72760751801b54b898706968
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-13 17:52:09 +0900

    feat: endpoint 사용 skeleton

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from app.api.endpoints import infos
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-
+app.include_router(infos.router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn

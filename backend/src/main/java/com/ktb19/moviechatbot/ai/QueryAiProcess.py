07952e6ba2be550e17154f023f8d6798fed4abdf
HJ'sHomeDesktop <goesy91@naver.com>
2024-08-14 14:48:36 +0900

    chore: openai api key초기화 삭제

--- backend/src/main/java/com/ktb19/moviechatbot/ai/QueryAiProcess.py
+++ backend/src/main/java/com/ktb19/moviechatbot/ai/QueryAiProcess.py
@@ -28,6 +28,13 @@ import Levenshtein
 from jamo import h2j, j2h, j2hcj
 from konlpy.tag import Okt
 
+import os
+from dotenv import load_dotenv
+# OPENAI_API_KEY
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY1')
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 # KoBERT 토크나이저와 모델 초기화
 tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 model = BertModel.from_pretrained('skt/kobert-base-v1')
@@ -94,11 +101,7 @@ def jamodict_search(query, jamodict):
     sorted_distance_list = sorted(distance_list, key=lambda x: min(x[2]))
     return sorted_distance_list
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY1')
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 # 메인 함수
 def process_documents_and_question(documents, question):
     # 한국 시간대 설정
@@ -159,10 +162,11 @@ def process_documents_and_question(documents, question):
         'today': today,
         'weekday': weekday
     })
-
+    # 기존에 입력한 영화 이름을 original에, full name을 movieName과 similar에
     responseDict = json.loads(response1)
-    print(responseDict)
-    if responseDict["similar"] is None:
+    if responseDict["movieName"] in query_results[1]:
+        return
+    elif responseDict["similar"] is None and responseDict["movieName"] is not None:
         movieNameQuery = jamodict_search(responseDict["movieName"],jamodict)
         response2 = chain2.invoke({
             'context': movieNameQuery,
@@ -182,55 +186,13 @@ def process_documents_and_question(documents, question):
         responseDict["original"] = responseDict["movieName"]
         responseDict["similar"] = responseReDict["movieName"]
         responseDict["movieName"] = responseReDict["movieName"]
+
     return json.dumps(responseDict)
 
 
 
 
-# 사용 예시
-# documents = [
-# "데드풀과 울버린",
-# "늘봄가든",
-# "빅토리",
-# "세븐틴 투어 ‘팔로우’ 어게인 투 시네마",
-# "명탐정 코난: 100만 달러의 펜타그램",
-# "2023 심규선 단독 콘서트 : 우리 앞의 세계",
-# "에이리언: 로물루스",
-# "토끼는 어디로 갔나요?",
-# "쥬라기캅스 극장판: 전설의 고대생물을 찾아라",
-# "행복의 나라",
-# "트위스터스",
-# "슈퍼배드 4",
-# "이준호 콘서트 : 다시 만나는 날",
-# "사랑의 하츄핑",
-# "우마무스메 프리티 더비 새로운 시대의 문",
-# "하이퍼포커스 : 투모로우바이투게더 브이알 콘서트",
-# "베베핀 플레이타임",
-# "바다 탐험대 옥토넛 어보브 앤 비욘드 : 바다가 위험해",
-# "이매지너리",
-# "탈주",
-# "2023 영탁 단독 콘서트 : 탁쇼2",
-# "헬로카봇 올스타 스페셜",
-# "탈출: 프로젝트 사일런스",
-# "극장총집편 봇치 더 록! 전편",
-# "플라이 미 투 더 문",
-# "볼빨간사춘기: 메리 고 라운드 더 무비",
-# "블랙핑크 월드투어 [본 핑크] 인 시네마",
-# "이솝이야기",
-# "박정희: 경제대국을 꿈꾼 남자",
-# "극장판 도라에몽: 진구의 지구 교향곡",
-# "2024 박은빈 팬 콘서트 <은빈노트 : 디바>",
-# "민요 첼로",
-# "다큐 황은정 : 스마트폰이 뭐길래"
-# ]
-#
-#
-# #%%
-# question = "울버린 토요일 5시에 보고 싶어"
-# result = process_documents_and_question(documents, question)
-# #%%
-# question = "데드풀 토요일 5시에 보고 싶어"
-# result = process_documents_and_question(documents, question)
+
 
 
 
@@ -240,10 +202,6 @@ json_response = process_documents_and_question(documents, question)
 
 # JSON 문자열을 파이썬 딕셔너리로 파싱
 entities = json.loads(json_response)
-
-# 클라이언트 초기화
-client = openai
-
 
 # 사용자에게 돌려줄 답변 생성
 def generate_response(entities):
@@ -303,9 +261,54 @@ def generate_response(entities):
     # json 형태로 변환하여 return
     return json.dumps(entities, ensure_ascii=False)
 
+# 사용 예시
+# documents = [
+# "데드풀과 울버린",
+# "늘봄가든",
+# "빅토리",
+# "세븐틴 투어 ‘팔로우’ 어게인 투 시네마",
+# "명탐정 코난: 100만 달러의 펜타그램",
+# "2023 심규선 단독 콘서트 : 우리 앞의 세계",
+# "에이리언: 로물루스",
+# "토끼는 어디로 갔나요?",
+# "쥬라기캅스 극장판: 전설의 고대생물을 찾아라",
+# "행복의 나라",
+# "트위스터스",
+# "슈퍼배드 4",
+# "이준호 콘서트 : 다시 만나는 날",
+# "사랑의 하츄핑",
+# "우마무스메 프리티 더비 새로운 시대의 문",
+# "하이퍼포커스 : 투모로우바이투게더 브이알 콘서트",
+# "베베핀 플레이타임",
+# "바다 탐험대 옥토넛 어보브 앤 비욘드 : 바다가 위험해",
+# "이매지너리",
+# "탈주",
+# "2023 영탁 단독 콘서트 : 탁쇼2",
+# "헬로카봇 올스타 스페셜",
+# "탈출: 프로젝트 사일런스",
+# "극장총집편 봇치 더 록! 전편",
+# "플라이 미 투 더 문",
+# "볼빨간사춘기: 메리 고 라운드 더 무비",
+# "블랙핑크 월드투어 [본 핑크] 인 시네마",
+# "이솝이야기",
+# "박정희: 경제대국을 꿈꾼 남자",
+# "극장판 도라에몽: 진구의 지구 교향곡",
+# "2024 박은빈 팬 콘서트 <은빈노트 : 디바>",
+# "민요 첼로",
+# "다큐 황은정 : 스마트폰이 뭐길래"
+# ]
+#
+#
+# #%%
+# question = "울버린 토요일 5시에 보고 싶어"
+# result = process_documents_and_question(documents, question)
+# #%%
+# question = "데드풀 토요일 5시에 보고 싶어"
 
 
 # 함수 실행 예시
+#result = process_documents_and_question(documents, question)
+#entities = json.loads(result)
 #json_response = generate_response(entities)
 #parsed_response = json.loads(json_response)
 #print(parsed_response)

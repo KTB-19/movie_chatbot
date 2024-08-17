e3a29a4129ddc5b5ee0f91274203be1817b0712d
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-17 13:32:26 +0900

    refactor: print를 logger로 수정

--- ai/app/api/endpoints/infos.py
+++ ai/app/api/endpoints/infos.py
@@ -6,35 +6,36 @@ from app.db.database import engine
 from app.models.info import Info
 from app.services.test1 import get_response
 from app.services.test2 import get_response_additional
+import logging
 
 router = APIRouter()
-
+logger = logging.getLogger("uvicorn")
 
 @router.get("/infos")
 async def get_infos(message: str = "") -> Info:
-    print("get_infos start")
-    print(message)
+    logger.info("get_infos start")
+    logger.info(f"message : {message}")
 
     movies = await read_movies()
-    print(movies)
+    logger.info(f"sql result movies : {movies}")
 
     response: Info = Info(**get_response(movies, message))
-    print(response)
+    logger.info(f"response : {response}")
 
     return response
 
 
 @router.post("/infos/additional")
 async def get_infos_additional(request: AdditionalInfosRequest) -> Info:
-    print("get_infos_additional start")
-    print(request.parsedQuery)
-    print(request.message)
+    logger.info("get_infos_additional start")
+    logger.info(f"parsedQuery : {request.parsedQuery}")
+    logger.info(f"message : {request.message}")
 
     movies = await read_movies()
-    print(movies)
+    logger.info(f"sql result movies : {movies}")
 
     response: Info = Info(**get_response_additional(movies, request.parsedQuery, request.message))
-    print(response)
+    logger.info(f"response : {response}")
 
     return response
 

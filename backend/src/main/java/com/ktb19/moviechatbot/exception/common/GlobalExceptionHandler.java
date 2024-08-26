b1c27e8c3b6782e333e3ebbba1d86f8e3a8e154d
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-26 21:16:00 +0900

    refactor: handlePyFunctionNotFoundException, handleFailParsingPyObjectToJsonException 비활성화

--- backend/src/main/java/com/ktb19/moviechatbot/exception/common/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ktb19/moviechatbot/exception/common/GlobalExceptionHandler.java
@@ -18,23 +18,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler(PyFunctionNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handlePyFunctionNotFoundException(PyFunctionNotFoundException e) {
-        log.error("PyFunctionNotFoundException functionName = {}", e.getRequestKey(), e);
-
-        ErrorCode errorCode = e.getCode();
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(ErrorResponse.of(errorCode));
-    }
-
-    @ExceptionHandler(FailParsingPyObjectToJsonException.class)
-    public ResponseEntity<ErrorResponse> handleFailParsingPyObjectToJsonException(FailParsingPyObjectToJsonException e) {
-        log.error("FailParsingPyObjectToJsonException pyObject = {}", e.getRequestKey(), e);
-
-        ErrorCode errorCode = e.getCode();
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(ErrorResponse.of(errorCode));
-    }
+//    @ExceptionHandler(PyFunctionNotFoundException.class)
+//    public ResponseEntity<ErrorResponse> handlePyFunctionNotFoundException(PyFunctionNotFoundException e) {
+//        log.error("PyFunctionNotFoundException functionName = {}", e.getRequestKey(), e);
+//
+//        ErrorCode errorCode = e.getCode();
+//        return ResponseEntity.status(errorCode.getHttpStatus())
+//                .body(ErrorResponse.of(errorCode));
+//    }
+//
+//    @ExceptionHandler(FailParsingPyObjectToJsonException.class)
+//    public ResponseEntity<ErrorResponse> handleFailParsingPyObjectToJsonException(FailParsingPyObjectToJsonException e) {
+//        log.error("FailParsingPyObjectToJsonException pyObject = {}", e.getRequestKey(), e);
+//
+//        ErrorCode errorCode = e.getCode();
+//        return ResponseEntity.status(errorCode.getHttpStatus())
+//                .body(ErrorResponse.of(errorCode));
+//    }
 
     @ExceptionHandler(FeignException.class)
     public ResponseEntity<ErrorResponse> handleFeignException(FeignException e) {

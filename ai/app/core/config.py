e9ddf82ea55540492a8977573243d8451a4183e3
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-28 16:52:11 +0900

    fix: config 수정

--- ai/app/core/config.py
+++ ai/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "ParseAI"
     DATABASE_URL: str = "mysql+aiomysql://root:qlalfqjsgh486@localhost:3306/moviedatabase"
-    OPEN_API_KEY: str
+    OPEN_API_KEY: str = ""
 
     class Config:
         env_file = ".env"

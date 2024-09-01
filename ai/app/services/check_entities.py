f12eb6e23ac21c87ac9822bda5d9a4fef12f8324
정예은 <79676210+Yeeun-Jeong@users.noreply.github.com>
2024-09-01 19:20:01 +0900

    chore: 필수 엔티티 개수 수정

--- ai/app/services/check_entities.py
+++ ai/app/services/check_entities.py
@@ -20,11 +20,11 @@ def check_json_entities(response):
 
 
 def check_entities(entities):
+    # 필수 엔티티 정보
     entity_info = [
         ("movieName", "영화 제목"),
         ("region", "지역"),
-        ("date", "날짜"),
-        ("time", "시간")
+        ("date", "날짜")
     ]
 
     missing_entities = []
@@ -32,16 +32,21 @@ def check_entities(entities):
         if key not in entities or not entities[key]:  # None 또는 False인 값을 걸러냄
             missing_entities.append(message)
 
-    # 엔티티가 2개 이상 누락된 경우
+    
     if missing_entities:
-        if len(missing_entities) >= 2:
+        # 엔티티가 하나 이상 채워지지 않은 경우 다시 질문하기
+        if len(missing_entities) > 1:
             missing_str = ' 와 '.join(missing_entities)
             user_message = f"관람하고 싶은 {missing_str}을 말씀해 주세요."
         else:
             user_message = f"관람하고 싶은 {missing_entities[0]}을 말씀해 주세요."
     else:
         # 엔티티가 모두 채워진 경우 확인 문장 출력
-        entities['date'], entities['time'] = format_date_time(entities['date'], entities['time'])   # 날짜, 시간 형식 변경 적용
-        user_message = f"{entities['date']} {entities['time']}에 {entities['region']}에서 {entities['movieName']}을(를) 보고 싶으신 게 맞으신가요?"
-
+        if "time" in entities and entities["time"]:
+            entities['date'], entities['time'] = format_date_time(entities['date'], entities['time'])   # 날짜, 시간 형식 변경 적용
+            user_message = f"{entities['date']} {entities['time']}에 {entities['region']}에서 {entities['movieName']}을(를) 보고 싶으신 게 맞으신가요?"
+        else:
+            entities['date'], _ = format_date_time(entities['date'], None)   # 사용자가 시간은 입력하지 않은 경우
+            user_message = f"{entities['date']}에 {entities['region']}에서 {entities['movieName']}을(를) 보고 싶으신 게 맞으신가요?"
+    
     return user_message, entities

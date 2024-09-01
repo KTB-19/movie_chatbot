ce255962cd57b44c5393faccab274c51764556d9
Hyunjoong_Kim(Ryan) <166325341+KHyunJoong@users.noreply.github.com>
2024-09-01 17:44:34 +0900

    Revert "날짜 포멧 수정 "

--- backend/src/main/java/com/ktb19/moviechatbot/repository/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ktb19.moviechatbot.repository;
-
-import com.ktb19.moviechatbot.entity.Theater;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheaterRepository extends JpaRepository<Theater, Long> {
-}
